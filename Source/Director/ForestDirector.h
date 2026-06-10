b30874573e9e5281d4f5f0f286aa520f14db55bc
agneau egaré <113533739+Domoticsagneau@users.noreply.github.com>
2026-06-10 17:19:29 +0200

    Update ForestDirector.h

---
+++ Source/Director/ForestDirector.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+
+#include "StoryTypes.h"
+
+#include "ForestDirector.generated.h"
+
+class ACameraManager;
+class AShadowActor;
+class ALeoCharacter;
+
+UCLASS()
+class LARUEDESMURMURES_API AForestDirector
+    : public AActor
+{
+    GENERATED_BODY()
+
+public:
+
+    AForestDirector();
+
+protected:
+
+    virtual void BeginPlay() override;
+
+public:
+
+    virtual void Tick(float DeltaTime) override;
+
+private:
+
+    FStoryData Story;
+
+    float CurrentTime;
+
+    int32 CurrentEventIndex;
+
+    void LoadStory();
+
+    void ExecuteEvent(
+        const FStoryEvent& Event);
+
+public:
+
+    UPROPERTY(EditAnywhere)
+    ACameraManager* CameraManager;
+
+    UPROPERTY(EditAnywhere)
+    AShadowActor* Shadow;
+
+    UPROPERTY(EditAnywhere)
+    ALeoCharacter* Leo;
+};
