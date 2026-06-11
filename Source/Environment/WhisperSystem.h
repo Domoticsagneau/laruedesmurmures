e63a463232a1e50155b25da2b1cffb18830e0b57
agneau egaré <113533739+Domoticsagneau@users.noreply.github.com>
2026-06-11 09:01:55 +0200

    Update WhisperSystem.h

--- Source/Environment/WhisperSystem.h
+++ Source/Environment/WhisperSystem.h
@@ -1,1 +1,29 @@
 
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "WhisperSystem.generated.h"
+
+class USoundBase;
+
+UCLASS()
+class LARUEDESMURMURES_API AWhisperSystem
+    : public AActor
+{
+    GENERATED_BODY()
+
+public:
+
+    UPROPERTY(EditAnywhere)
+    TArray<USoundBase*> WhisperSounds;
+
+    UFUNCTION()
+    void PlayWhisper(
+        FVector Location);
+
+    UFUNCTION()
+    void PlayRandomWhisper(
+        FVector AroundLocation,
+        float Radius);
+};
