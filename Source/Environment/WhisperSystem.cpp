57c50096064f3d2fa2d6bd6ad96b7d01d72233df
agneau egaré <113533739+Domoticsagneau@users.noreply.github.com>
2026-06-11 09:03:29 +0200

    Update WhisperSystem.cpp

--- Source/Environment/WhisperSystem.cpp
+++ Source/Environment/WhisperSystem.cpp
@@ -1,1 +1,54 @@
+#include "WhisperSystem.h"
+
+#include "Kismet/GameplayStatics.h"
+
+void AWhisperSystem::PlayWhisper(FVector Location)
+{
+    if (WhisperSounds.Num() == 0)
+    {
+        return;
+    }
+
+    UGameplayStatics
+    ::PlaySoundAtLocation(
+        this,
+        WhisperSounds[0],
+        Location);
+}
+
+void AWhisperSystem::PlayRandomWhisper(
+    FVector AroundLocation,
+    float Radius)
+{
+    if (WhisperSounds.Num() == 0)
+    {
+        return;
+    }
+
+    FVector Offset;
+
+    Offset.X =
+        FMath::RandRange(
+            -Radius,
+             Radius);
+
+    Offset.Y =
+        FMath::RandRange(
+            -Radius,
+             Radius);
+
+    Offset.Z = 0.f;
+
+    int32 Index =
+        FMath::RandRange(
+            0,
+            WhisperSounds.Num()-1);
+
+    UGameplayStatics
+    ::PlaySoundAtLocation(
+        this,
+        WhisperSounds[Index],
+        AroundLocation + Offset);
+}
+
 
